c26196620dc91a9f44d15558218d6e5e988a52ca
25Miki <mariamarchantellopis@gmail.com>
2023-05-10 20:48:28 +0200

    miercoles clase

--- src/styleguide/views-sg/Styleguide.jsx
+++ src/styleguide/views-sg/Styleguide.jsx
@@ -16,21 +16,33 @@ export default function Styleguide() {
 
       <div className="section">
         <h3>Variable Colors</h3>
-        <div className="color__circle">
+        <div className="color__circle lighter">
           <p>50</p>
           <p>#FDEDE7</p>
         </div>
-        <div className="color__circle">
+        <div className="color__circle light">
           <p>100</p>
           <p>#FCDCCF</p>
         </div>
-        <div className="color__circle">
+        <div className="color__circle regular">
           <p>200</p>
           <p>#F9B99F</p>
         </div>
-        <div className="color__circle">
+        <div className="color__circle dark">
           <p>300</p>
-          <p>##F7A787</p>
+          <p>#F7A787</p>
+        </div>
+          <div className="color__circle darker">
+          <p>400</p>
+          <p>#F6956F</p>
+        </div>
+          <div className="color__circle sdark">
+          <p>500</p>
+          <p>#F3733F</p>
+        </div>
+          <div className="color__circle ssdark">
+          <p>600</p>
+          <p>#F04F0F</p>
         </div>
       </div>
       <Nav />
